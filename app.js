33eba3af1122ffe25e4ef7b740d52d61e8bf1f44
Dante Hawler <202559257+D-Hawler@users.noreply.github.com>
2025-10-09 22:56:30 +0300

    initial error handling

--- app.js
+++ app.js
@@ -9,6 +9,13 @@ app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/", index);
 
+app.use((err, req, res, next) => {
+  console.error(err);
+
+  res.status(500);
+  res.render("error.views.ejs");
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, (err) => {
   if (err) throw err;
